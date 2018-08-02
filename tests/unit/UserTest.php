ba93d03ca66955d824d46b029edfccb904270b18
EDDY-EDDY <eddy.udom@pglnigeria.com>
2018-08-02 23:17:11 +0100

    Second commit

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eddyu.
+ * Date: 02/08/2018
+ * Time: 10:43 PM
+ */
+
+class UserTest extends PHPUnit\Framework\TestCase
+{
+
+    private $first_name  = "John";
+    private $last_name = "Doe";
+    private $email = "info@pglnigeria.com";
+
+
+    public function testFirstName()
+    {
+
+        $user = new \App\Entity\User();
+
+        $user->setFirstName($this->first_name);
+
+        $this->assertEquals($user->getFirstName(), $this->first_name);
+
+    }
+
+    public function testLastName()
+    {
+
+        $user = new \App\Entity\User();
+
+        $user->setLastName($this->last_name);
+
+        $this->assertEquals($user->getLastName(), $this->last_name);
+
+    }
+
+    public function testEmail()
+    {
+
+        $user = new \App\Entity\User();
+
+        $user->setEmail($this->email);
+
+        $this->assertEquals($user->getEmail(), $this->email);
+
+    }
+
+    public function testIsActive()
+    {
+
+        $user = new \App\Entity\User();
+
+        $user->setIsActive(1);
+
+        $this->assertEquals($user->getisActive(), '1');
+
+    }
+
+
+    public function testRoles()
+    {
+        $user = new \App\Entity\User();
+
+        $user->setRoles('ROLE_USER');
+
+        $this->assertContains('ROLE_USER', $user->getRoles());
+    }
+
+}
